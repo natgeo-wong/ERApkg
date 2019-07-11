2336884da59bf02caf80e621c8169846ced5b4fb
Nathanael Wong <natgeo.wong@outlook.com>
2019-07-11 23:49:58 +0800

    More Tm functions

--- scripts/era_Tm.m
+++ scripts/era_Tm.m
@@ -1,4 +1,4 @@
-%                                era_PI.m
+%                                era_Tm.m
 %          Script to find PI constant that transforms ZWD to PWV
 %                          Nathanael Wong Zhixin
 %
@@ -11,15 +11,15 @@
 % - Manandhar et al. [2017]
 
 clear all; [ ini,root,logf ] = era_startup(2,2);
-[ mod,par,trange,reg ] = era_initialize(ini,5,3,1);
+[ mod,par,tvec,reg ] = era_initialize(ini,5,3,1);
 
-fprintf('Proceeding to run Tm script ...\n\n'); ID = era_Tm_ID(2);
+fprintf('Proceeding to run Tm script ...\n\n');
 
-if     ID.ID == 1,          fol = era_Tm_Davis(mod,par,reg,trange,root,ini);
-elseif any(ID.ID == [2 3]), fol = era_Tm_Bevis(mod,par,reg,root,ini,ID.ID);
-elseif ID.ID == 4,          fol = era_Tm_GGOS (mod,par,reg,trange,root,ini);
-elseif ID.ID == 5,          fol = era_Tm_GPT2w(mod,par,reg,trange,root);
-elseif ID.ID == 6,          fol = era_Tm_GTm3 (mod,par,reg,trange,root);
+if     ID == 1,          fol = era_Tm_Davis(mod,par,reg,tvec,root,ini);
+elseif any(ID == [2 3]), fol = era_Tm_Bevis(mod,par,reg,tvec,root,ini,ID);
+elseif ID == 4,          fol = era_Tm_GGOS (mod,par,reg,tvec,root);
+elseif ID == 5,          fol = era_Tm_GPT2w(mod,par,reg,tvec,root);
+elseif ID == 6,          fol = era_Tm_GTm3 (mod,par,reg,tvec,root);
 end
 
 era_shutdown(logf,fol.raw); clear all;
