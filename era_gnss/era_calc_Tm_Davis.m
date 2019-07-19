a5879243587dc411f98df0fe3ea054b4ecca2840
Nathanael Wong <natgeo.wong@outlook.com>
2019-07-19 21:52:49 +0800

    Changed integration, may need extra pressure multiplication

--- era_gnss/era_calc_Tm_Davis.m
+++ era_gnss/era_calc_Tm_Davis.m
@@ -5,7 +5,7 @@ function Tm = era_calc_Tm_Davis (Ta,sH,p,Ts)
 Rd = 287.05; Rv = 461.51; ep = Rd / Rv;
 p(end+1) = 1012.35; p = reshape(p,1,1,1,[]);
 Ta = cat(4,Ta,Ts); sH = cat(4,sH,sH(:,:,:,37));
-e = (p*100) .* ((1-sH) ./ (1-sH + sH/ep));
+e = (p.^2) .* ((1-sH) ./ (1-sH + sH/ep));
 
 p = p(:); top = e ./ Ta; bot = e ./ Ta.^2;
 
