11dfff64a5a7ea7c863bca392561c98c0c4c7467
Nathanael Wong <natgeo.wong@outlook.com>
2019-07-21 00:43:43 +0800

    Added surface pressure to pressure vector only AFTER concatenating variables together

--- era_calc/era_calc_Tm_Davis_pd.m
+++ era_calc/era_calc_Tm_Davis_pd.m
@@ -2,11 +2,11 @@ function Tm = era_calc_Tm_Davis_pd (p,Ta,Ts,Td,sH)
 
 
 
-Rd = 287.05; Rv = 461.51; ep = Rd / Rv;
-p(end+1) = 1012.35; p = reshape(p,1,1,1,[]);
+Rd = 287.05; Rv = 461.51; ep = Rd / Rv; p = reshape(p,1,1,1,[]);
 e = p .* sH ./ ((1-sH)*ep + sH); e38 = era_calc_Td2e(Td);
 Ta = cat(4,Ta,Ts); e = cat(4,e,e38);
 
+p(end+1) = 1012.35; p = reshape(p,1,1,1,[]);
 top = e ./ p; bot = e ./ (p .* Ta);
 
 p = p(:); top = cumtrapz(p,top,4); bot = cumtrapz(p,bot,4); Tm = top./bot;
--- era_calc/era_calc_Tm_Davis_zd.m
+++ era_calc/era_calc_Tm_Davis_zd.m
@@ -3,10 +3,11 @@ function Tm = era_calc_Tm_Davis_zd (p,Ta,Ts,Td,sH,za,zs)
 
 
 Rd = 287.05; Rv = 461.51; ep = Rd / Rv;
-p(end+1) = 1012.35; p = reshape(p,1,1,1,[]); np = size(p,4);
+p = reshape(p,1,1,1,[]);
 e = p .* sH ./ ((1-sH)*ep + sH); e38 = era_calc_Td2e(Td);
 Ta = cat(4,Ta,Ts); za = cat(4,za,zs); e = cat(4,e,e38);
 
+p(end+1) = 1012.35; np = size(p,4);
 top = e ./ Ta; bot = e ./ Ta.^2; dim = size(top); Tm = zeros(dim);
 top = reshape(top,[],np)'; bot = reshape(bot,[],np)';
 za = reshape(za,[],np)'; Tm = reshape(Tm,[],np)'; npt = size(Tm,1);
