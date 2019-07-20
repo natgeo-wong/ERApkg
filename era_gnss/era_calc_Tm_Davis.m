5e63d475a7bef523923999b7fff134489b77a850
Nathanael Wong <natgeo.wong@outlook.com>
2019-07-20 14:20:46 +0800

    Further modifications to Tm_Davis calculation

--- era_gnss/era_calc_Tm_Davis.m
+++ era_gnss/era_calc_Tm_Davis.m
@@ -5,10 +5,10 @@ function Tm = era_calc_Tm_Davis (Ta,sH,p,Ts)
 Rd = 287.05; Rv = 461.51; ep = Rd / Rv;
 p(end+1) = 1012.35; p = reshape(p,1,1,1,[]);
 Ta = cat(4,Ta,Ts); sH = cat(4,sH,sH(:,:,:,37));
-e = (p.^2) .* ((1-sH) ./ (1-sH + sH/ep));
+e = p .* ((1-sH) ./ (1-sH + sH/ep)); cnv = p ./ Ta;
 
-p = p(:); top = e ./ Ta; bot = e ./ Ta.^2;
+top = cnv .* e ./ Ta; bot = cnv .* e ./ Ta.^2;
 
-top = cumtrapz(p,top,4); bot = cumtrapz(p,bot,4); Tm = top ./ bot;
+p = p(:); top = cumtrapz(p,top,4); bot = cumtrapz(p,bot,4); Tm = top./bot;
 
 end
