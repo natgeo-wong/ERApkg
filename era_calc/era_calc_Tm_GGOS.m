1f64f6dc89bc6312c792714f01b4bec9e91e9395
#NATHANAEL WONG ZHIXIN# <natgeo.wong@outlook.com>
2019-07-22 18:03:13 +0800

    Changed final result grid call to matrix

--- era_calc/era_calc_Tm_GGOS.m
+++ era_calc/era_calc_Tm_GGOS.m
@@ -3,13 +3,11 @@ function Tm_out = era_calc_Tm_GGOS (Tm_in,lon_G,lat_G,mlon,mlat)
 
 
 nt = size(Tm_in,3); nlon = numel(mlon); nlat = numel(mlat);
-Tm_out = zeros([nlon nlat nt]); Tm_out = reshape(Tm_out,[],nt);
+Tm_out = zeros([nlon nlat nt]);
 
 parfor ii = 1 : nt, Tmii = Tm_in(:,:,ii);
-    Tm_out(:,ii) = interp2(lat_G,lon_G,Tmii,mlat,mlon,'makima');
-    %Tm_out(:,ii) = NaN;
+    Tm_out(:,:,ii) = interp2(lat_G,lon_G,Tmii,mlat,mlon,'makima');
+    %Tm_out(:,:,ii) = NaN;
 end
-
-Tm_out = reshape(Tm_out,[nlon nlat nt]);
 
 end
