f6f5b6964de9704c6fc8da54250c204e58ad05c0
#NATHANAEL WONG ZHIXIN# <natgeo.wong@outlook.com>
2019-07-22 18:12:33 +0800

    Added try-catch for 1997 which is all NaN

--- era_calc/era_calc_Tm_GGOS.m
+++ era_calc/era_calc_Tm_GGOS.m
@@ -6,8 +6,9 @@ nt = size(Tm_in,3); nlon = size(mlon,1); nlat = size(mlon,2);
 Tm_out = zeros([nlon nlat nt]);
 
 parfor ii = 1 : nt, Tmii = Tm_in(:,:,ii);
-    Tm_out(:,:,ii) = interp2(lat_G,lon_G,Tmii,mlat,mlon,'makima');
-    %Tm_out(:,:,ii) = NaN;
+    try    Tm_out(:,:,ii) = interp2(lat_G,lon_G,Tmii,mlat,mlon,'makima');
+    catch, Tm_out(:,:,ii) = NaN;
+    end
 end
 
 end
