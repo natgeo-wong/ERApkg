664ce32458d044512ebb8067dadda4e3c95ad416
#NATHANAEL WONG ZHIXIN# <natgeo.wong@outlook.com>
2019-07-22 17:51:23 +0800

    Made more adjustments to interpolation

--- era_calc/era_calc_Tm_GGOS.m
+++ era_calc/era_calc_Tm_GGOS.m
@@ -2,14 +2,14 @@ function Tm_out = era_calc_Tm_GGOS (Tm_in,lon_G,lat_G,lon,lat)
 
 
 
-nt = size(Tm_in,3); dim = size(mlon); Tm_out = zeros([dim nt]);
-Tm_out = reshape(Tm_out,[],nt);
+nt = size(Tm_in,3); nlon = numel(lon); nlat = numel(lat);
+Tm_out = zeros([nlon nlat nt]); Tm_out = reshape(Tm_out,[],nt);
 
-parfor ii = 1 : nt, Tmii = Tm_in(:,:,ii); Tmii = Tmii(:);
-    Tm_out(:,ii) = interp2(lon_G,lat_G,Tmii,lon,lat,'makima');
-    Tm_out(:,ii) = NaN;
+parfor ii = 1 : nt, Tmii = Tm_in(:,:,ii);
+    Tm_out(:,ii) = interp2(lat_G,lon_G,Tmii,lat,lon,'makima');
+    %Tm_out(:,ii) = NaN;
 end
 
-Tm_out = reshape(Tm_out,[dim nt]);
+Tm_out = reshape(Tm_out,[nlon nlat nt]);
 
 end
